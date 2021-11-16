d6668e8cca343a98bc4070606c28df138ced57c2
unknown <shiv_kumar.98@outlook.com>
2021-11-16 09:23:47 +0000

    restarting from scracth on validateISBN and tests

--- src/com/isbntools/ValidateISBN.java
+++ src/com/isbntools/ValidateISBN.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class ValidateISBN {
 
-	public boolean checkISBN(String isbn) {
+	public static boolean checkISBN(String isbn) {
 		
 		String[] stringArray = isbn.split("");
 		int[] integerArray = new int[stringArray.length];
@@ -13,23 +13,37 @@ public class ValidateISBN {
 			throw new NumberFormatException();
 		}
 		
+		
+		
 		for (int j =0; j<stringArray.length;j++) {
-			integerArray[j] = Integer.parseInt(stringArray[j]) ;
+			if (j!=9) {
+				if (!Character.isDigit(stringArray.toString().charAt(j))) {
+				throw new NumberFormatException("please provide ISBN with only digits");
+				} else {
+					integerArray[j]=Integer.parseInt(stringArray[j]) ;
+				}
+			} else { // j=9
+				if (stringArray.toString().charAt(j)== 'X') {
+					integerArray[j] = 10;
+				} else {
+				integerArray[j] = Integer.parseInt(stringArray[j]) ;
+				}
+			}
 		}
 		
 		int sum = 0;
-		sum = sum + 10*integerArray[0]
-				+ 9*integerArray[1]
-				+ 8*integerArray[2]
-				+ 7*integerArray[3]
-				+ 6*integerArray[4]
-				+ 5*integerArray[5]
-				+ 4*integerArray[6]
-				+ 3*integerArray[7]
-				+ 2*integerArray[8]
-				+ 1*integerArray[9];
-								
-						
+		
+			sum = sum +10*integerArray[0]
+					+ 9*integerArray[1]
+					+ 8*integerArray[2]
+					+ 7*integerArray[3]
+					+ 6*integerArray[4]
+					+ 5*integerArray[5]
+					+ 4*integerArray[6]
+					+ 3*integerArray[7]
+					+ 2*integerArray[8]
+					+ 1*integerArray[9];
+					
 		System.out.println(sum);
 		int remainder = sum % 11;
 		
@@ -41,6 +55,12 @@ public class ValidateISBN {
 		}
 	
 		
+	}
+	
+	public static void main(String[] args) {
+		checkISBN("0198534540");
+		
+		System.out.println(Integer.parseInt("X"));
 	}
 
 }
--- test/com/isbntools/ValidateISBNTest.java
+++ test/com/isbntools/ValidateISBNTest.java
@@ -14,27 +14,5 @@ public class ValidateISBNTest {
 		assertTrue(result);
 	}
 	
-	@Test
-	public void checkThatAnotherISBN_isValid() {
-		ValidateISBN validator = new ValidateISBN();
-		boolean result = validator.checkISBN("0198534530");
-
-	}
-	
-	@Test
-	public void checkAnInvalidISBN_returnsFalse() {
-		ValidateISBN validateISBN=new ValidateISBN();
-		assertFalse(validateISBN.checkISBN("0198534540"));
-	}
-	
-	
-	
-
-	
-	@Test(expected = NumberFormatException.class)
-	public void checkNineDigitISNBThrowsException() {
-		ValidateISBN validateISBN=new ValidateISBN();
-		assertFalse(validateISBN.checkISBN("123456789"));
-	}
 
 }
